fcb18a0594670b1cd7727475ca7e42f3809c427b
Mark Edomwande <mark.edomwande@andela.com>
2019-01-15 14:26:33 +0100

    hardcoded the cell towers

--- google/index.js
+++ google/index.js
@@ -7,11 +7,39 @@ app.controller('MainCtrl', function($scope) {
   var defaultCell = [{
     "id": 1,
     "cellTowers": [{
-      "cellId": '',
-      "locationAreaCode": '',
-      "mobileCountryCode": '',
-      "mobileNetworkCode": '',
-      "signalStrength": -60
+      "cellId": 9247,
+      "locationAreaCode": 12710,
+      "mobileCountryCode": 255,
+      "mobileNetworkCode": 6,
+      "signalStrength": 49
+    }]
+  },
+  {
+    "id": 2,
+    "cellTowers": [{
+      "cellId": 9341,
+      "locationAreaCode": 12710,
+      "mobileCountryCode": 255,
+      "mobileNetworkCode": 6,
+      "signalStrength": 28
+    }]
+  },{
+    "id": 3,
+    "cellTowers": [{
+      "cellId": 9243,
+      "locationAreaCode": 12710,
+      "mobileCountryCode": 255,
+      "mobileNetworkCode": 6,
+      "signalStrength": 26
+    }]
+  },{
+    "id": 4,
+    "cellTowers": [{
+      "cellId": 9401,
+      "locationAreaCode": 12710,
+      "mobileCountryCode": 255,
+      "mobileNetworkCode": 6,
+      "signalStrength": 17
     }]
   }];
 
@@ -76,6 +104,7 @@ app.controller('MainCtrl', function($scope) {
   $scope.cellTowerSearch = function() {
     removeMarkers();
     geolocator('cells', $scope.cells, $scope.cells.length);
+
   }
 
 
